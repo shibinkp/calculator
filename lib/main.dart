53e51f5429ffbec93580db902dc872910ea0d195
fasalmbt <mfmbt786@gmail.com>
2019-08-14 13:25:45 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: "Calculator App",
+      title: "Calculator",
       theme: new ThemeData(primarySwatch: Colors.red),
       home: new HomePage(),
     );
